d49a0a04a4ccb7dc9bed542857ef1f8375ad5c88
ê¹k-haechan <gibbm1127@naver.com>
2026-01-22 09:47:42 +0900

    init

---
+++ src/test/java/ssafy17/team4/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package ssafy17.team4.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
